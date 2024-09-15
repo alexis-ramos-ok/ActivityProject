7758980360763b80b65578262954f7a31a086a4e
Alexis Ramos <alexleaderhere@gmail.com>
2024-09-15 16:44:59 -0300

    Ejercicio 3

--- Assets/Script/PracticaDos.cs
+++ Assets/Script/PracticaDos.cs
@@ -7,7 +7,6 @@ public class PracticaDos : MonoBehaviour
     // Función comparadora que toma tres valores flotantes
     public string Comparador(float a, float b, float c)
     {
-
         // Determina el mayor y el menor de los valores tomados
         float mayor = Mathf.Max(a, b, c);
         float menor = Mathf.Min(a, b, c);
@@ -46,9 +45,44 @@ public class PracticaDos : MonoBehaviour
         Debug.Log(resultado);
     }
 
-    // Update is called once per frame
+    //  Array de 10 números enteros dado y temporizador para hacer la modificación
+    private int[] numeros = { 97, -64, -3, -58, -15, 58, 51, 38, -31, -37 };
+    private float timer = 0f;
+
+    // Variable de control para asegurar que tanto la modificación como la impresión
+    // se hagan una sola vez
+    private bool arrayModificado = false;
+
     void Update()
     {
+        // Incrementa el temporizador con el tiempo transcurrido desde el último frame
+        timer += Time.deltaTime;
 
+        // Espera 4.5 segundos antes de modificar el array
+        if (timer >= 4.5f && !arrayModificado)
+        {
+            // Llamar a la función que duplica los pares pasando el array como parámetro
+            DuplicadorDePares(numeros);
+            arrayModificado = true;
+
+            // Convierte el array a string e imprime en consola
+            string numerosString = string.Join(", ", numeros);
+            Debug.Log("Array con valores de números pares duplicados: [" + numerosString + "]");
+        }
+    }
+
+    // Función que recibe un array de enteros y duplica los valores pares,
+    // los valores modificados se asignan al mismo array
+    void DuplicadorDePares(int[] array)
+    {
+        // Recorre el array y duplica los números pares
+        for (int i = 0; i < array.Length; i++)
+        {
+            // Si el número es par (divisible por 2 sin resto), duplicamos
+            if (array[i] % 2 == 0)
+            {
+                array[i] *= 2;
+            }
+        }
     }
 }
